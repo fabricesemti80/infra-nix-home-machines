694d8b9826a01fd0820825aa6cca30020f3123f8
Fabrice-on-NixOS <fabrice@fabricesemti.com>
2024-12-11 12:00:59 +0000

    duplicated definition

--- home/fs/nixos.nix
+++ home/fs/nixos.nix
@@ -9,12 +9,9 @@
     ../modules/zsh.nix       
   ];
 
-  # Enable home-manager
-  programs.home-manager.enable = true;
-
   # Nicely reload system units when changing configs
   systemd.user.startServices = "sd-switch";
-  
+
   # Home Manager needs a bit of information about you and the paths it should
   # manage.
   home.username = "fs";
