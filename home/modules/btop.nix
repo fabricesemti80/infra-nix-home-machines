a2277fe73028682025ecb13c6a131a19ee02a700
Fabrice Semti <fabrice@fabricesemti.com>
2024-12-21 17:43:58 +0000

    Update btop.nix

--- home/modules/btop.nix
+++ home/modules/btop.nix
@@ -2,9 +2,11 @@
   # Install btop via home-manager module
   programs.btop = {
     enable = true;
-    catppuccin.enable = true;
     settings = {
       vim_keys = true;
     };
   };
+
+  # New top-level Catppuccin configuration
+  catppuccin.btop.enable = true;
 }
