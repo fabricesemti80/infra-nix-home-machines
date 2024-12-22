725e5064c0cb2e29845c91245012a07e085f6ac2
Fabrice Semti <fabrice@fabricesemti.com>
2024-12-22 09:42:46 +0000

    rename user avatar

--- flake.nix
+++ flake.nix
@@ -40,7 +40,6 @@
       url = "github:zsh-users/zsh-history-substring-search";
       flake = false;
     };
-
   };
 
   outputs = {
@@ -57,19 +56,19 @@
     inherit (self) outputs;
 
     # Define user configurations
-    users = {  
-        fs = {
-          avatar = ./files/avatar/face;
-          email = "fabrice@fabricesemti.com";
-          fullName = "Fabrice Semti";
-          gitKey = "YOUR_GIT_KEY";
-          name = "fs";
-          sshKeys = [
-            "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBJpVWYmXPpqVmlHdixDR//vdfD+sryvYmpH2Dj1/Otx fabrice@fabricesemti.com"
-            "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDl0ivGFV8D/M53/qvRRkfxkKgY3635xDiiLQwFgrWon fabrice@fabricesemti.com"
-            "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIX2Y3nmVHNxNCNV+WXHeBEcXFS0XYDnNWxzm9oAIyFa fabrice@fabricesemti.com"
-          ];
-        };      
+    users = {
+      fs = {
+        avatar = ./files/avatar/face.png;
+        email = "fabrice@fabricesemti.com";
+        fullName = "Fabrice Semti";
+        gitKey = "YOUR_GIT_KEY";
+        name = "fs";
+        sshKeys = [
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBJpVWYmXPpqVmlHdixDR//vdfD+sryvYmpH2Dj1/Otx fabrice@fabricesemti.com"
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDl0ivGFV8D/M53/qvRRkfxkKgY3635xDiiLQwFgrWon fabrice@fabricesemti.com"
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIX2Y3nmVHNxNCNV+WXHeBEcXFS0XYDnNWxzm9oAIyFa fabrice@fabricesemti.com"
+        ];
+      };
     };
 
     # Function for NixOS system configuration
