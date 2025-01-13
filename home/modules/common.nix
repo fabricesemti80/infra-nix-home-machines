ee49770078ee96c7172e05b9ab14a805ab8186bd
fabrice-on-VM <fabrice@fabricesemti.com>
2025-01-13 20:45:42 +0000

    feat: :zap: enable CLI apps

--- home/modules/common.nix
+++ home/modules/common.nix
@@ -2,20 +2,20 @@
   imports = [
     # ../modules/alacritty.nix
     ../modules/atuin.nix
-    # ../modules/bat.nix
-    # ../modules/btop.nix
+    ../modules/bat.nix
+    ../modules/btop.nix
     ../modules/fastfetch.nix
     ../modules/fzf.nix
     ../modules/git.nix
     # ../modules/go.nix
     # ../modules/gpg.nix
     # # ../modules/krew.nix #! causes errors
-    # ../modules/lazygit.nix
-    # ../modules/neovim.nix
+    ../modules/lazygit.nix
+    ../modules/neovim.nix
     # ../modules/saml2aws.nix
     ../modules/scripts.nix
     ../modules/starship.nix
-    # ../modules/tmux.nix
+    ../modules/tmux.nix
     ../modules/zsh.nix
     # ../modules/wezterm.nix
   ];
