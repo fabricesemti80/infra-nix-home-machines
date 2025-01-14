d977047316836a9a7b90ef235d251a2fa4e85d35
Fabrice Semti <fabrice@fabricesemti.com>
2025-01-14 13:41:25 +0000

    Update jankyborders.nix

--- hosts/modules/jankyborders.nix
+++ hosts/modules/jankyborders.nix
@@ -1,32 +1,5 @@
-{ pkgs, lib, ... }: {
-  homebrew = {
-    taps = [
-      "FelixKratz/formulae"
-    ];
+{ config, lib, pkgs, ... }:
 
-    brews = [
-      "borders"
-    ];
-  };
-
-  services.borders = let
-    cfg = config.services.borders;
-  in {
-    enable = true;
-    options = [
-      "active_color=0xffe1e3e4"
-      "inactive_color=0xff494d64"
-      "width=5.0"
-    ];
-  };
-
-  launchd.user.agents.borders = {
-    serviceConfig = {
-      ProgramArguments = [ "/usr/local/bin/borders" ] ++ config.services.borders.options;
-      KeepAlive = true;
-      RunAtLoad = true;
-      StandardOutPath = "/tmp/borders.log";
-      StandardErrorPath = "/tmp/borders.error.log";
-    };
-  };
+{
+  services.jankyborders.enable = true;
 }
