69cca803757340c9455a3f2e4605326bf8c32083
Fabrice Semti <fabrice@fabricesemti.com>
2025-01-14 11:07:03 +0000

    Update jankyborders.nix

--- hosts/modules/jankyborders.nix
+++ hosts/modules/jankyborders.nix
@@ -1,1 +1,30 @@
-#
+{ pkgs, ... }: {
+
+  homebrew = {
+
+    # Homebrew package types:
+    # - brews: Command line tools and libraries, installed from source/binary, located in /usr/local/Cellar
+    # - casks: GUI applications with graphical interfaces, pre-built binaries, installed to /Applications
+
+    # Homebrew Additional Repositories
+    taps = [
+      "FelixKratz/formulae"
+    ];
+
+    brews = [
+      "borders"          # Highlight borders - https://github.com/FelixKratz/JankyBorders?tab=readme-ov-file#jankyborders
+    ];
+
+    # Enable the borders service
+    services.borders = {
+      enable = true;
+      options = [
+        "active_color=0xffe1e3e4"
+        "inactive_color=0xff494d64"
+        "width=5.0"
+      ];
+    };
+
+  };
+
+}
