262cc85d6c5369be564cdf1092d9e7579105b16c
Fabrice Semti <fabrice@fabricesemti.com>
2025-01-27 10:53:34 +0000

    clean Dock

--- hosts/macpro-fs/configuration.nix
+++ hosts/macpro-fs/configuration.nix
@@ -122,12 +122,12 @@
 
           ## direct installs
           "/Applications/Brave Browser.app"
-          "/Applications/OrbStack.app"
+          # "/Applications/OrbStack.app"
           "/Applications/Ghostty.app"
 
-          ## Home Manager packages
-          "${pkgs.vscode}/Applications/Visual Studio Code.app"
-          "${pkgs.obsidian}/Applications/Obsidian.app"
+          # ## Home Manager packages
+          # "${pkgs.vscode}/Applications/Visual Studio Code.app"
+          # "${pkgs.obsidian}/Applications/Obsidian.app"
 
         ];
         tilesize = 60; # Dock icon size
@@ -187,30 +187,6 @@
     "
   '';
 
-  # # System packages to install
-  # environment = {
-  #   systemPackages = with pkgs; [
-  #     (python3.withPackages (ps: with ps; [pip virtualenv])) # Python with common packages
-  #     bartender # Menu bar organization
-  #     colima # Docker alternative for macOS
-  #     delta # Better git diff
-  #     docker # Container platform
-  #     du-dust # Disk usage analyzer
-  #     eza # Modern ls replacement
-  #     fd # Find alternative
-  #     home-manager # User environment manager
-  #     jq # JSON processor
-  #     just # Command runner
-  #     kubectl # Kubernetes CLI
-  #     lazydocker # Docker TUI
-  #     nh # Nix helper
-  #     obsidian # Note-taking app
-  #     openconnect # VPN client
-  #     pipenv # Python environment manager
-  #     ripgrep # Fast grep alternative
-  #     vscode # Code editor
-  #   ];
-  # };
 
   # Enable Zsh as the default shell
   programs.zsh.enable = true;
@@ -221,25 +197,6 @@
     nerd-fonts.meslo-lg
     roboto
   ];
-
-  # # Configure Homebrew
-  # homebrew = {
-  #   enable = true;
-  #   casks = [
-  #     # GUI applications to install via Homebrew
-  #     "1password"
-  #     "aerospace" # Window manager
-  #     # "anki" # Flashcard app
-  #     "brave-browser" # Web browser
-  #     "obs" # Streaming software
-  #     "raycast" # Spotlight replacement
-  #   ];
-  #   taps = [
-  #     # Additional Homebrew repositories
-  #     "nikitabobko/tap"
-  #   ];
-  #   onActivation.cleanup = "zap"; # Aggressive cleanup of unused packages
-  # };
 
   # Set hostname
   networking.hostName = "macpro-fs";
