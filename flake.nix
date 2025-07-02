1d6b5a2526a3e4cd2173735d668777b42ffc7452
Fabrice Semti <fabrice@fabricesemti.com>
2025-07-02 18:13:22 +0100

    refactor(nix): Move user configuration to separate file
    This commit refactors the Nix configuration by moving the user configuration for 'fs' from `flake.nix` to a dedicated file `users/fs.nix`. This change promotes modularity and improves the organization of the Nix configuration, making it easier to manage and maintain user-specific settings.

--- flake.nix
+++ flake.nix
@@ -55,20 +55,9 @@
   } @ inputs: let
     inherit (self) outputs;
 
-    # Define user configurations
+    # Define user configurations by importing from separate files for modularity and privacy
     users = {
-      fs = {
-        avatar = ./files/avatar/face.png;
-        email = "fabrice@fabricesemti.com";
-        fullName = "Fabrice Semti";
-        gitKey = "YOUR_GIT_KEY";
-        name = "fs";
-        sshKeys = [
-          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBJpVWYmXPpqVmlHdixDR//vdfD+sryvYmpH2Dj1/Otx fabrice@fabricesemti.com"
-          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDl0ivGFV8D/M53/qvRRkfxkKgY3635xDiiLQwFgrWon fabrice@fabricesemti.com"
-          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIX2Y3nmVHNxNCNV+WXHeBEcXFS0XYDnNWxzm9oAIyFa fabrice@fabricesemti.com"
-        ];
-      };
+      fs = import ./users/fs.nix;
     };
 
     # Function for NixOS system configuration
---
+++ users/fs.nix
@@ -0,0 +1,12 @@
+{
+  avatar = ../files/avatar/face.png;
+  email = "fabrice@fabricesemti.com";
+  fullName = "Fabrice Semti";
+  gitKey = "YOUR_GIT_KEY";
+  name = "fs";
+  sshKeys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBJpVWYmXPpqVmlHdixDR//vdfD+sryvYmpH2Dj1/Otx fabrice@fabricesemti.com"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDl0ivGFV8D/M53/qvRRkfxkKgY3635xDiiLQwFgrWon fabrice@fabricesemti.com"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIX2Y3nmVHNxNCNV+WXHeBEcXFS0XYDnNWxzm9oAIyFa fabrice@fabricesemti.com"
+  ];
+}
